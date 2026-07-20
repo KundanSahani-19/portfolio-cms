a80c9e64aa46090d380a7359db05024e1eaf620c
kundan kumar sahani <kundankumarsahani@kundans-MacBook-Air.local>
2026-07-20 21:15:14 +0530

    Remove hardcoded education data

--- client/src/components/sections/education/educationData.js
+++ client/src/components/sections/education/educationData.js
@@ -1,22 +1,3 @@
-const educationData = [
-  {
-    year: "2020",
-    degree: "10th (CBSE)",
-    institute: "St. Martin's Public School",
-    score: "60%",
-  },
-  {
-    year: "2022",
-    degree: "12th",
-    institute: "New Adarsh High Secondary School",
-    score: "67.2%",
-  },
-  {
-    year: "2023 - 2027",
-    degree: "B.Tech Computer Science & Engineering",
-    institute: "ITM University, Gwalior",
-    score: "CGPA 7.4",
-  },
-];
+const educationData = [];
 
 export default educationData;
